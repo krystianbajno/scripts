e3c9cae9b265dc5640f1eb70381879819e74cdf9
Krystian Bajno <krystian.bajno@gmail.com>
2024-09-01 21:37:21 +0200

    Update file-contents.sh

--- file-contents.sh
+++ file-contents.sh
@@ -1,1 +1,7 @@
 find . -type f -exec sh -c 'echo "===== {} ====="; cat "{}"; echo ""' \; | pbcopy
+
+Get-ChildItem -Recurse -File | ForEach-Object {
+    Write-Output "===== $($_.FullName) ====="
+    Get-Content $_.FullName
+    Write-Output ""
+} | Set-Clipboard
