1b2716b6e72432e63710e0711fcd82fe4178ca62
k_b <krystian.bajno@gmail.com>
2025-04-11 19:11:44 +0200

    Update git-commit-blame.sh

--- bash/git-commit-blame.sh
+++ bash/git-commit-blame.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
+is_mac() {
+    [[ "$(uname)" == "Darwin" ]]
+}
+
+is_linux() {
+    [[ "$(uname)" == "Linux" ]]
+}
+
+convert_date() {
+    local commit_date="$1"
+    
+    if is_mac; then
+        formatted_date=$(date -j -f "%a %b %d %H:%M:%S %Y" "$commit_date" "+%Y-%m-%d %H:%M:%S")
+    elif is_linux; then
+        formatted_date=$(date -d "$commit_date" "+%Y-%m-%d %H:%M:%S")
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
+
+    echo "$formatted_date"
+}
+
 commit_history=$(git log --pretty=format:"%H %cd" --date=local)
 
 while read -r line; do
     commit_hash=$(echo $line | awk '{print $1}')
     commit_date=$(echo $line | awk '{$1=""; print $0}' | sed 's/^ *//')
+
+    commit_date=$(convert_date "$commit_date")
 
     commit_day=$(date -d "$commit_date" +"%u")
     commit_hour=$(date -d "$commit_date" +"%H")
